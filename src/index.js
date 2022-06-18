79656660001b4b9cbe88024ab4ea1908ea58550c
thetechrat <ogunyemialameen@gmail.com>
2022-06-18 06:01:54 -0700

    add netlify folder

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { MoralisProvider } from "react-moralis";
 
-
 ReactDOM.render(
   <React.StrictMode>
-    <MoralisProvider appId="xxxxxxxx" serverUrl="xxxxxxxx">
+    <MoralisProvider
+      appId="ph51Onv275VOFHF0zqNEMXV8BRlcq0y1G5IXm5ar"
+      serverUrl="https://wu6fdkmd93vz.usemoralis.com:2053/server"
+    >
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </MoralisProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
